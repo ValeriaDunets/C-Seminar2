12024caace2f89ecad28e18dcccefb451ed7d706
Valeria <valeriadunets>
2023-04-26 13:51:11 +0900

    HomeWork2

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿ // Задача 10
+ /*Console.WriteLine ("Input your number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+void ShowDec () 
+{
+if (number > 99 & number < 1000) // 456
+{
+    int digit = (number % 100 - number % 10);
+    int new_num = digit/10;
+    Console.WriteLine ($"Your second number {number} is a {new_num}");
+
+}
+else
+{
+    Console.WriteLine($"Your number {number} is not three-digit number");
+}
+}
+ShowDec ();
+*/
+//Задача 13
+//Напшиите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+/*void ThirdNum ()
+{
+    Console.WriteLine("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 99)
+    {
+        int digit = number % 10;
+        Console.WriteLine($"Your third number {number} is a {digit}");
+    }
+    else
+    {
+        Console.WriteLine($"No third digit");
+    } 
+}
+ThirdNum ();
+*/
+
+// Задача 15
+//Напишите программу,  которая принимает на вход цифру, обозначающую день недели и проверяет, является ли этот день выходным
+
+/*void DayWeek ()
+{
+    Console.WriteLine("Input your number of week");
+    int day = Convert.ToInt32(Console.ReadLine());
+    if (day > 5  & day < 8)
+    {
+        Console.WriteLine($"Your day {day} YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
+DayWeek ();
+*/
